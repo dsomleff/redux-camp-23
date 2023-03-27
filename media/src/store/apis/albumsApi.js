f2fe080725c4116737662562dd03789141afe2ec
somleff <somleff@gmail.com>
2023-03-27 13:13:07 +0200

    Creating an Endpoint

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -1,6 +1,26 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const albumsApi = createApi({
-  // key to store all state related to this API in a main redux store
-  reducerPath: 'albums',
+    // key to store all state related to this API in a main redux store
+    reducerPath: 'albums',
+    //fetchBaseQuery config func, wrapper around basic fetch
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005',
+    }),
+    endpoints(builder) {
+        return {
+            //fetchAlbums will be the name of generated hook
+            fetchAlbums: builder.query({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id,
+                        },
+                        method: 'GET',
+                    };
+                },
+            }),
+        };
+    }
 });
