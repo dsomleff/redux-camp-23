6a41de62dda9204e860cf36ad792427739ffbdef
somleff <somleff@gmail.com>
2023-03-30 11:10:42 +0200

    Changing Data with Mutations

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -1,4 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
 
 const albumsApi = createApi({
     // key to store all state related to this API in a main redux store
@@ -22,9 +23,21 @@ const albumsApi = createApi({
                     };
                 },
             }),
+            addAlbum: builder.mutation({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        method: 'POST',
+                        body: {
+                            userId: user.id,
+                            title: faker.commerce.productName(),
+                        },
+                    };
+                },
+            }),
         };
     }
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
